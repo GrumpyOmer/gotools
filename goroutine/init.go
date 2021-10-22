fa003adea8cad8d6530624cbd7eb4a7645d7b165
omer <lovemj353286116@qq.com>
2021-10-22 19:35:43 +0800

    feat: add new goroutine manager

---
+++ goroutine/init.go
@@ -0,0 +1,72 @@
+package Goroutine
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+type (
+	queueStruct struct {
+		function  func(*sync.WaitGroup) error
+		waitGroup *sync.WaitGroup
+	}
+	goroutineManager struct {
+		*sync.Mutex
+		max     int64              // 最大协程数
+		current int64              // 当前正在运行的协程
+		queue   chan (queueStruct) // 装载 当前/等待 执行的任务
+	}
+)
+
+var (
+	manager goroutineManager
+)
+
+// 初始化
+func init() {
+	// 初始化通道 默认缓冲1000
+	manager.queue = make(chan queueStruct, 1000)
+	for v := range manager.queue {
+		go func(function func(*sync.WaitGroup) error, w *sync.WaitGroup) {
+			defer func() {
+				// 执行结束修改当前协程数信息 原子操作保证一致性
+				atomic.AddInt64(&manager.current, -1)
+				if err := recover(); err != nil {
+					// 记录任务错误 防止进程重启
+					fmt.Printf("recover(): %v\n", recover())
+				}
+			}()
+			// running task
+			function(w)
+		}(v.function, v.waitGroup)
+	}
+}
+
+// 设置最大协程数
+func SetGoroutineNumber(max int64) {
+	manager.max = max
+}
+
+// 生成协程任务
+func MakeTask(task func(*sync.WaitGroup) error, w *sync.WaitGroup) error {
+	manager.Mutex.Lock()
+	defer manager.Mutex.Unlock()
+	if manager.current == manager.max {
+		return errors.New("当前协程数量已达限制")
+	}
+	// 更新当前协程数信息 原子操作保证一致性
+	atomic.AddInt64(&manager.current, 1)
+	// 任务写入通道
+	manager.queue <- queueStruct{
+		function:  task,
+		waitGroup: w,
+	}
+	return nil
+}
+
+//查看当前堆积任务
+func SearchCurTask() int {
+	return len(manager.queue)
+}
