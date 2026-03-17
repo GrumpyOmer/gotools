35506c801cb5ff81387ab0f5bcfb417880eb5035
GrumpyOmer <GrumpyOmer@gmail.com>
2026-03-17 17:39:47 +0800

    feat: optimize logCenter

--- logCenter/init.go
+++ logCenter/init.go
@@ -26,61 +26,70 @@ var (
 	// 默认10W记录缓存
 	channel = make(chan stackInfo, 100000)
 	// 是否将日志强制写入磁盘，默认不强制
-	fSync         bool
-	fSyncRWMutex  sync.RWMutex
+	fSync        bool
+	fSyncMutex   sync.RWMutex
+	// 文件操作相关的并发安全控制
+	fileMutex    sync.Mutex   // 保护文件操作的互斥锁
+	currentFile  *os.File     // 当前日志文件句柄
+	currentDate  string       // 当前日志文件对应的日期
+	logDir       = "./log"   // 日志目录
 )
 
 func init() {
 	fmt.Println("log center init")
-	// 默认10个消费者
-	for i := 0; i < 10; i++ {
-		go func() {
-			for {
-				data := <-channel
-				func() {
-					str, _ := json.Marshal(data)
-					fileName := time.Now().Format("20060102")
-					var (
-						filename = "./log/" + fileName + ".text"
-						f        *os.File
-						err1     error
-					)
-					if _, err := os.Stat(filename); os.IsNotExist(err) {
-						if err1 = os.MkdirAll("./log", 0777); err1 != nil {
-							fmt.Println(err1.Error())
-							return
-						}
-						f, err1 = os.Create(filename) //创建文件
-					} else {
-						f, err1 = os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0666) //打开文件
-					}
-					if err1 != nil {
-						fmt.Println(err1.Error())
-						return
-					}
-					defer func() {
-						if err1 = f.Close(); err1 != nil {
-							fmt.Println("关闭文件失败，err：" + err1.Error())
-						}
-					}()
-					_, err1 = f.WriteString(string(str) + "\n") //写入文件(字符串)
-					if err1 != nil {
-						fmt.Println(err1.Error())
-						return
-					}
-					fSyncRWMutex.RLock()
-					shouldSync := fSync
-					fSyncRWMutex.RUnlock()
-					if shouldSync {
-						// 刷入磁盘
-						if err1 = f.Sync(); err1 != nil {
-							fmt.Println("刷入磁盘失败，err：" + err1.Error())
-						}
-						//fmt.Println("刷入磁盘")
-					}
-				}()
-			}
-		}()
+	// 启动时确保日志目录存在
+	if err := os.MkdirAll(logDir, 0777); err != nil {
+		fmt.Println("创建日志目录失败：", err.Error())
+	}
+	// 单消费者模式：日志写入是IO密集型，单消费者避免文件竞争，更高效
+	go func() {
+		for {
+			data := <-channel
+			writeLog(data)
+		}
+	}()
+}
+
+// writeLog 写入日志（线程安全）
+func writeLog(data stackInfo) {
+	str, _ := json.Marshal(data)
+	today := time.Now().Format("20060102")
+
+	fileMutex.Lock()
+	defer fileMutex.Unlock()
+
+	// 检查是否需要切换文件（日期变化或文件未打开）
+	if currentDate != today || currentFile == nil {
+		// 关闭旧文件
+		if currentFile != nil {
+			currentFile.Close()
+		}
+		// 打开新文件
+		filename := logDir + "/" + today + ".text"
+		f, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+		if err != nil {
+			fmt.Println("打开日志文件失败：", err.Error())
+			return
+		}
+		currentFile = f
+		currentDate = today
+	}
+
+	// 写入日志
+	_, err := currentFile.WriteString(string(str) + "\n")
+	if err != nil {
+		fmt.Println("写入日志失败：", err.Error())
+		return
+	}
+
+	// 检查是否需要刷盘
+	fSyncMutex.RLock()
+	shouldSync := fSync
+	fSyncMutex.RUnlock()
+	if shouldSync {
+		if err := currentFile.Sync(); err != nil {
+			fmt.Println("刷入磁盘失败：", err.Error())
+		}
 	}
 }
 
@@ -98,7 +107,7 @@ func Add(content string) {
 }
 
 func SaveFSync(o bool) {
-	fSyncRWMutex.Lock()
+	fSyncMutex.Lock()
 	fSync = o
-	fSyncRWMutex.Unlock()
+	fSyncMutex.Unlock()
 }
